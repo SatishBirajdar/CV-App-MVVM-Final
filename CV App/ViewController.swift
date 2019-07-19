07175989133ae207e322e3ec97d79d212dccf81b
Satish Birajdar <satishbirajdar1@gmail.com>
2019-07-18 20:04:55 -0400

    Initial Commit

---
+++ CV App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CV App
+//
+//  Created by Satish Birajdar on 2019-07-18.
+//  Copyright © 2019 SBSoftwares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CV AppTests/CV_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  CV_AppTests.swift
+//  CV AppTests
+//
+//  Created by Satish Birajdar on 2019-07-18.
+//  Copyright © 2019 SBSoftwares. All rights reserved.
+//
+
+import XCTest
+@testable import CV_App
+
+class CV_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
